5b9fb3f117375ca455250dddbfc4790f5a627ba0
User <User@DESKTOP-U7IM74J>
2021-07-29 19:16:38 +0900

    Commit

---
+++ src/ch05_1_reference_variable/StringEqualsExample.java
@@ -0,0 +1,33 @@
+package ch05_1_reference_variable;
+
+public class StringEqualsExample {
+	public static void main(String[] args) {
+		
+		String strVar1 = "신민철";
+		String strVar2 = "신민철";
+		
+		if(strVar1 == strVar2) {
+			System.out.println("strVar1과 strVar2가 참조가 같음");
+		} else {
+			System.out.println("strVar1과 strVar2가 참조가 다름");
+		}
+		
+		if(strVar1.equals(strVar2)) {
+			System.out.println("strVar1과  strVar2가 문자열이 같음");
+		}
+		
+		String strVar3 = new String("신민철");
+		String strVar4 = new String("신민철");
+		
+		if(strVar3 == strVar4) {
+			System.out.println("strVar3과 strVar4가 참조가 같음");
+		} else {
+			System.out.println("strVar3과 strVar4가 참조가 다름");
+		}
+		
+		if(strVar3.equals(strVar4));
+				 {
+			System.out.println("strVar3과 strVar4가 문자열이 같음");
+		}
+	}
+}
